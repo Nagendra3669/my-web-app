61b7788bc3830d556233c375a9958f84820d6d2e
Nagendra3669 <nagendraanaparthi@hotmail.com>
2026-05-29 12:14:25 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Welcome to My Web App</h1>
+        <p>Deployed using Azure DevOps CI/CD</p>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
